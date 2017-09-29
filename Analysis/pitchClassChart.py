be3dac54624e899cf49c89a51831a68b48fcca29
cjwit <chris.witulski@gmail.com>
2017-09-29 10:42:36 -0400

    fix pitch chart and range chart x-axis numbers

--- Analysis/pitchClassChart.py
+++ Analysis/pitchClassChart.py
@@ -67,7 +67,7 @@ def appendToCSV( csv, pitchClassDurations ):
 
 def buildCSV( songs ):
 	"Build a CSV file from a list of songs"
-	csv = {0:[],1:[],2:[],3:[],4:[],5:[],6:[],7:[],8:[],9:[],10:[],11:[]}
+	csv = {11:[],10:[],9:[],8:[],7:[],6:[],5:[],4:[],3:[],2:[],1:[],0:[]}
 	metadata = {"titles": [], "numbers": [], "movements":[]}
 	outputText = "Songs"
 	for index,song in enumerate(songs):
